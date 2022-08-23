6356f5dbea1338def2e87097074ec65d9b0f05ba
Ahmed Gürkan Özçelik <ahmetgozcelik@gmail.com>
2022-08-23 13:59:16 +0300

    The transitions of the questions were arranged.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ Soru.prototype.cevabiKontrolEt = function(cevap) {
 
 
 let sorular = [
-    new Soru("1-Hangisi javascript paket yönetim  uygulamasıdır?", {a: "Node.js", b: "Typescript", c: "Npm"}, "c"),
+    new Soru("1-Hangisi javascript paket yönetim  uygulamasıdır?", {a: "Node.js", b: "Typescript", c: "Npm", d: "Nuget"}, "c"),
     new Soru("2-Hangisi javascript paket yönetim  uygulamasıdır?", {a: "Node.js", b: "Typescript", c: "Npm"}, "c"),
     new Soru("3-Hangisi javascript paket yönetim  uygulamasıdır?", {a: "Node.js", b: "Typescript", c: "Npm"}, "c"),
     new Soru("4-Hangisi javascript paket yönetim  uygulamasıdır?", {a: "Node.js", b: "Typescript", c: "Npm"}, "c")
@@ -30,11 +30,31 @@ const quiz = new Quiz(sorular);
 
 
 document.querySelector(".btn_start").addEventListener("click", function(){
-    if(quiz.sorular.length != quiz.soruIndex){
-        document.querySelector(".quiz_box").classList.add("active");
-        console.log(quiz.soruGetir());
-    quiz.soruIndex += 1;
+    document.querySelector(".quiz_box").classList.add("active");
+    soruGoster(quiz.soruGetir());
+});
+
+document.querySelector(".next_btn").addEventListener("click", function(){
+    if(quiz.sorular.length != quiz.soruIndex + 1){
+        quiz.soruIndex += 1;
+        soruGoster(quiz.soruGetir());
     } else {
         console.log("quiz bitti");
     }
-});
+})
+
+function soruGoster(soru) {
+    let question = `<span>${soru.soruMetni}</span>`;
+    let options = '';
+
+    for(let cevap in soru.cevapSecenekleri) {
+        options += 
+        `
+            <div class="option">
+                <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
+            </div>
+        `;
+    }
+    document.querySelector(".question_text").innerHTML = question;
+    document.querySelector(".option_list").innerHTML = options;
+}
